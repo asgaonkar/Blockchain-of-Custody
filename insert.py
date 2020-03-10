b80444133577be6a66b97b9bb9ebb35cc497d823
Atit S Gaonkar <atit.sgaonkar@gmail.com>
2020-03-09 19:24:19 -0700

    Corrected Added Item print Statement

--- insert.py
+++ insert.py
@@ -71,7 +71,7 @@ def insert(case_id, item_id, file_path):
         fp.write(packed_data_values)
         fp.close()
 
-        print("Added item: ", i)
+        print("Added item:", i)
         print("\tStatus: CHECKEDIN")
         print("\tTime of action:", datetime.now().strftime(
             '%Y-%m-%dT%H:%M:%S.%f') + 'Z')
